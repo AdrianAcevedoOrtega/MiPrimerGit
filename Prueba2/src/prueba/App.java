c51a10f6604c658def9fa7ed362bf1e30a2b99e9
ALUMNO2425 <ALUMNO2425@DESKTOP-CDGKK4P>
2026-03-23 11:08:08 +0100

    Primer Comit rama develop

--- Prueba2/src/prueba/App.java
+++ Prueba2/src/prueba/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		//System.out.println("Hola mundo");
 		System.out.println("Hola mundoGIT");
+		System.out.println("Rama develop");
 
 	}
 
