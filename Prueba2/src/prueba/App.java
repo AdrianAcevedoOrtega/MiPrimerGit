c88ad84721449a5aecb643aa39c613796c2aaa41
ALUMNO2425 <ALUMNO2425@DESKTOP-CDGKK4P>
2026-03-23 10:44:15 +0100

    Cambiando el mensaje de salida. "Hola mundo" -> "Hola mundoGIT".

--- Prueba2/src/prueba/App.java
+++ Prueba2/src/prueba/App.java
@@ -3,7 +3,8 @@ package prueba;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		//System.out.println("Hola mundo");
+		System.out.println("Hola mundoGIT");
 
 	}
 
